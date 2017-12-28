96083d814913e0e8bdd27030b536199e1b4bb577
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-28 18:30:54 +0100

    OOP First Exemple
    This is my first exemple in OOP in C#

---
+++ OOP_FirstExemple/OOP_FirstExemple/Vehicule.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_FirstExemple
+{
+    public class Vehicule
+    {
+        /// <summary>
+        /// This value represents the car company
+        /// </summary>
+        /// <remarks>The company name</remarks>
+        /// <value>Cars company names</value>
+        public string manufecuterer
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string model
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string color
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int engineSize
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Color functions
+        /// </summary>
+        /// <remarks>To change the car color</remarks>
+        public void painting()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Method()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
