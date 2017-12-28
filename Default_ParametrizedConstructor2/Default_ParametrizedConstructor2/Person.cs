1e917322fe75ecf300be74056209515a05da683b
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-28 19:52:56 +0100

    Default and Parametrized Constructor
    This is default and parametrized constructor course in OOP C#

---
+++ Default_ParametrizedConstructor2/Default_ParametrizedConstructor2/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Default_ParametrizedConstructor2
+{
+    class Person
+    {
+        string name;
+        int age;
+        public void PrintValue() { Console.WriteLine($"{name}, {age}"); }
+       //Default
+        public Person()
+        {
+            name = "Moncef";
+            age = 26;
+        }
+        //Parametrized constructor
+        public Person(string value)
+        {
+            name = value;
+            age = 26;
+        }
+        //Parametrized constructor
+        public Person(string value2, int myAge)
+        {
+            name = value2;
+            age = myAge;
+        }
+    }
+}
---
+++ Default_ParametrizedConstructor2/Default_ParametrizedConstructor2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Default_ParametrizedConstructor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Mounir";
+            int age2 = 22;
+            string myName2 = "Badis";
+
+            Person myObject = new Person();
+            myObject.PrintValue();
+            Console.WriteLine();
+
+            Person myObject2 = new Person(myName, age2);
+            myObject2.PrintValue();
+            Console.WriteLine();
+
+            Person myObject3 = new Person(myName2);
+            myObject3.PrintValue();
+
+            Console.ReadKey();
+        }
+    }
+}
