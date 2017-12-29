552e4d960cd4115305040a4816837edd196eccd2
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-29 20:04:16 +0100

    Static Classes
    This is static classes course in OOP C#

---
+++ StaticClasses/StaticClasses/Employees.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClasses
+{
+    static class Employees
+    {
+        static public string name = "Mncef Oussaf";
+        static public int age = 26;
+
+        static public void PrintName()
+        {
+            Console.WriteLine($"Name : {name} , Age : {age}");
+        }
+    }
+}
---
+++ StaticClasses/StaticClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employees.PrintName();
+            Console.ReadKey();
+        }
+    }
+}
