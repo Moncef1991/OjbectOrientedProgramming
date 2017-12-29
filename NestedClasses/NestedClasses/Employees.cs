d24c6d6bad04cee8a236937f4673c81a3696d2b3
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2017-12-29 19:46:52 +0100

    Nested Classes
    This is nested classes course in OOP C#

---
+++ NestedClasses/NestedClasses/Employees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedClasses
+{
+    class Employees
+    {
+        public string fName = "Oussaf Moncef";
+        public string adress = " City Chikhi";
+        public void PrintValues() { Console.WriteLine($"Employee name is {fName} \nLiving at {adress}"); }
+        public class Departements
+        {
+            public string departementName = "administration";
+            public void PrintDepartementName() { Console.WriteLine($"Working at {departementName}"); }
+        }
+    }
+}
---
+++ NestedClasses/NestedClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees myEmployee = new Employees();
+            myEmployee.PrintValues();
+            Employees.Departements myDepart = new Employees.Departements();
+            myDepart.PrintDepartementName();
+
+            Console.ReadKey();
+        }
+    }
+}
