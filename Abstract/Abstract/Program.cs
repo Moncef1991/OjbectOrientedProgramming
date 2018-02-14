cfaebb892bce25bee27550910f3f2c009dca0072
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2018-02-14 17:13:36 +0100

    Abstract Class
    This is abstract class course in OOP C#

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Managers myMang = new Managers();
+            HRD myHRD = new HRD();
+
+            myMang.name = "Monsif";
+            myHRD.name = "Badis";
+
+            myMang.PrintName();
+            myMang.PrintDepartment();
+
+            myHRD.PrintName();
+            myHRD.PrintDepartment();
+
+            Console.ReadKey();
+        }
+    }
+}
