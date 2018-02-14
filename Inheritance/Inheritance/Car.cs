cb81d4cc57aa3e4542979440f8613261d4555b62
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2018-02-14 17:16:28 +0100

    Inheritance
    This is inheritance course in OOP C#

---
+++ Inheritance/Inheritance/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Car
+    {
+        //The relation is "is a kind"
+        public string model="Camery";
+        public string make="Toyota";
+        public string year;
+        public void PrintModel()
+        {
+            Console.WriteLine($"{model}");
+        }
+
+
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toyota myObject = new Toyota();
+            myObject.make = "Ford";
+            myObject.year = "2013";
+            myObject.PrintModel();
+            myObject.PrintEngineSize();
+            Console.WriteLine($"{myObject.make}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
