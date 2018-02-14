58445f17174ae2d1c3a12a0f89e8b166b04ef81c
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2018-02-14 17:17:46 +0100

    Inheritance Class Diagram
    This is inheritance class diagram course in OOP C#

---
+++ InheritanceClassDiagram/InheritanceClassDiagram/Cars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InheritanceClassDiagram
+{
+    public class Cars
+    {
+        public string model="RAV4";
+        public string make="Toyota";
+        public int year=2012;
+
+        public void PrintModel()
+        {
+            Console.WriteLine($"{model}");
+        }
+    }
+}
---
+++ InheritanceClassDiagram/InheritanceClassDiagram/Toyota.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InheritanceClassDiagram
+{
+    public class Toyota : Cars
+    {
+        public int engineSize=3;
+
+        public void PrintEngineSize()
+        {
+            Console.WriteLine($"{engineSize}");
+        }
+    }
+}
