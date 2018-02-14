1416b9f282bd38d20faf3bbebf9f2c8c19ae8e6c
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2018-02-14 17:15:37 +0100

    Encapsulation Applications
    This is encapsulation course in OOP C#

---
+++ encapsulationApp/encapsulationApp/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulationApp
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public void PrintValues()
+        {
+            Console.WriteLine($"Name = {name} Age = {age}");
+        }
+
+        //Constructors
+        public Person() { }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        //Setters and getters methods
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public void setAge(int myAge)
+        {
+            age = myAge;
+        }
+        public int getAge()
+        {
+            return age ;
+        }
+    }
+}
---
+++ encapsulationApp/encapsulationApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Monsif Oussaf";
+            int age = 26;
+            Person myObject = new Person();
+            myObject.setName(name);
+            myObject.setAge(age);
+
+            Console.WriteLine("Name is " + myObject.getName());
+            Console.WriteLine("Age is " + myObject.getAge());
+            Console.ReadKey();
+        }
+    }
+}
