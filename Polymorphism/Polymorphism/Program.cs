c69639369e2d78b4ae4022de104ecfa6ffa2bfda
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2018-02-15 14:42:59 +0100

    Polymorphisim
    This is polymorphisim course in OOP C#

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars myCarsObject = new Cars();
+            Toyota myToyotaObject = new Toyota();
+            Nissan myNissanObject = new Nissan();
+            Ford myFordObject = new Ford();
+
+            myCarsObject.PrintMake();
+            myFordObject.PrintMake();
+            myNissanObject.PrintMake();
+            myToyotaObject.PrintMake();
+
+            Console.ReadKey();
+
+        }
+    }
+}
