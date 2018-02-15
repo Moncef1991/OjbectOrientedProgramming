b467ddfb7a9c9d8546c591a03bc4e8efe8e4d844
Moncef1991 <33940516+Moncef1991@users.noreply.github.com>
2018-02-15 15:39:39 +0100

    Sealed Classes
    This is sealed classes course in OOP C#

---
+++ SealedClasses/SealedClasses/PTEmployees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClasses
+{
+    sealed class PTEmployees : Employee
+    {
+        public string shift;
+        public override void PrintName()
+        {
+            Console.WriteLine($"{name}");
+        }
+        public override void PrintDepart()
+        {
+            Console.WriteLine($"{department = "PTEmplyees"}");
+        }
+    }
+}
---
+++ SealedClasses/SealedClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Managers myMang = new Managers();
+            HR myHRD = new HR();
+            PTEmployees myPTE = new PTEmployees();
+
+            myMang.name = "Monsif";
+            myHRD.name = "Badis";
+
+            myMang.PrintName();
+            myMang.PrintDepart();
+
+            myHRD.PrintName();
+            myHRD.PrintDepart();
+            myPTE.shift = "Day";
+            Console.WriteLine($"{myPTE.shift}");
+
+            Console.ReadKey();
+        }
+    }
+}
